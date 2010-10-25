2b441532ffe973153f260a62dbc8baae135e7b64
mvgompel <mvgompel@12f355fe-0486-481a-ad91-c297ab22b4e3>
2010-10-25 18:42:10 +0000

    Added SRILM to install script
    git-svn-id: https://ilk.uvt.nl/svn/trunk/sources/pbmbmt/releases/0.34@6652 12f355fe-0486-481a-ad91-c297ab22b4e3

--- install.sh
+++ install.sh
@@ -124,3 +124,44 @@ else
 fi
 
 
+
+echo "Looking for SRILM Python module..."
+if [ -f pyblpl/lm/srilmcc.so ]; then
+    echo "Found!"
+else
+    echo "Downloading SRILM base code (full version available at: http://www-speech.sri.com/projects/srilm/), licensed under SRILM Research Community License)"
+    mkdir tmp
+
+    wget http://ilk.uvt.nl/~mvgompel/srilm-5.10-pymod.tar.gz
+    tar -xvzf srilm-5.10-pymod.tar.gz
+    cd srilm-5.10-pymod
+    export SRILM=`pwd`
+    MACHINE_TYPE="i686"
+    make MACHINE_TYPE=$MACHINE_TYPE NO_TCL=X
+    export SRILMLIBS=$SRILM/lib/$MACHINE_TYPE
+    
+    if [ -d /usr/include/python2.6 ]; then
+        PYTHONVERSION="2.6"
+    elif [ -d /usr/include/python2.7 ]; then
+        PYTHONVERSION="2.7"
+    elif [ -d /usr/include/python2.5 ]; then
+        PYTHONVERSION="2.5"
+    else
+        echo "Unable to find python headers in /usr/include/python2.5 / 2.6 / 2.7. "
+        exit 1
+    fi
+
+    g++ -fPIC -shared -I/usr/include/python$PYTHONVERSION -lpython$PYTHONVERSION -I$SRILM/src -I$SRILM/include -lboost_python srilm.cc $SRILMLIBS/liboolm.a $SRILMLIBS/libdstruct.a $SRILMLIBS/libmisc.a -o srilmcc.so #this assumes python libraries are in /usr/include/python !
+
+    cd ..
+    rm -Rf tmp
+
+fi
+
+if [ -z "$TIMBL" ]; then TIMBL=`which timbl`; fi
+if [ -z "$TIMBL" ]; then
+
+else
+    echo "Timbl found: $TIMBL"
+fi
+
